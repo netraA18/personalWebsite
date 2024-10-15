636155646f7f2fc0e6ff82720b622737dbb90525
netraA18 <netraamin13@gmail.com>
2024-10-14 21:44:51 -0400

    Dark is default now

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import theme from './theme';
 
 
 
@@ -11,7 +12,7 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <ColorModeScript />
     <App />
     </ChakraProvider>
