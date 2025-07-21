f7b12b8a5c2fa1f8b46a53c30c8e8a8da346aa2e
netraA18 <netraamin13@gmail.com>
2025-07-21 19:44:32 -0400

    added new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,9 +6,12 @@ import { CheckCircleIcon } from '@chakra-ui/icons';
 import WeatherChef from '../WeatherChef.png';
 import lyricsCard from '../lyricsCard.png';
 import itunesGallery from '../itunesGallery.png';
-import flappy from '../flappySS.png';
-import todoSS from '../todoSS.png';
-import brickBreaker from '../brickbreaker.png';
+// import flappy from '../flappySS.png';
+// import todoSS from '../todoSS.png';
+// import brickBreaker from '../brickbreaker.png';
+import microblogging_platform from '../microblogging_platform.png';
+import business_health from '../business_health.png';
+import medElly from '../medElly.png';
 
 function Cards(props) {
 
@@ -88,16 +91,21 @@ function Cards(props) {
 function Projects() {
 
   const data = [
-    {title: "Weather Chef", description: "Developed a dynamic web application integrating recipe search and weather-based recipe recommendations. Implemented RESTful JSON APIs for seamless data retrieval. Provides ingredients, videos, and instructiosn.", language:"Javascript (React.js)", design:"HTML, CSS, and MaterialUI", wordToHighlight: ['dynamic', 'APIs', 'integrating', 'videos,'], image: WeatherChef, projLink: 'https://weather-chef.vercel.app/'},
+
+       {title: "Microblogging Platform", description: "Collaborated in a team to develop a full-stack microblogging web application usingSpring Boot and a MySQL backend. Built features including user authentication, posting with hashtags, following users, liking/commenting/bookmarking posts, profile views, hashtag search, and more", language: "MySQL, Spring Boot, Docker, Maven", wordToHighlight: ['full-stack', 'users,', 'authentication,', 'search,'], image: microblogging_platform, projLink: 'dd'},
+        {title: "Business Health", description: "Collaborated in a team to develop a full-stack web application offer access to financial data and user-contributed reviews for publicly traded companies. Displays net margin, quick ratio, and other key metrics. Shows a list of alumni from that company. ", language: "Next.js, Tailwind, Firebase", wordToHighlight: ['financial', 'companies.', 'net', 'margin,', 'alumni'], image: business_health, projLink: 'https://swe-rosy.vercel.app/'},
+        {title: "MedElly", description: "MedElly is a personalized music therapy platform that helps individuals through frequency-based music, since studies show that music at specific frequencies can aid memory recall. ", language: "Typescript, Howler.js, Next.js, and Auth0", wordToHighlight: ['frequency-based', 'music,'], image: medElly, projLink: 'https://devpost.com/software/uga-hacks-x'},
+            {title: "Weather Chef", description: "Developed a dynamic web application integrating recipe search and weather-based recipe recommendations. Implemented RESTful JSON APIs for seamless data retrieval. Provides ingredients, videos, and instructions.", language:"Javascript (React.js)", design:"HTML, CSS, and MaterialUI", wordToHighlight: ['dynamic', 'APIs', 'integrating', 'videos,'], image: WeatherChef, projLink: 'https://weather-chef.vercel.app/'},
     {title: "Lyrics Card", description: "Initiated this project to address the gap in personalized lyric cards available online. Users can customize the text content, font styles, and colors on the canvas, as well as upload an image for the song cover to download the card!", language: "JavaScript (with Fabric.js)", design:"HTML and CSS", wordToHighlight: ['download', 'address', 'styles,', 'Initiated'], image: lyricsCard, projLink: 'https://lyrics-card.vercel.app/'},
     {title: "Itunes Gallery", 
       description:
        "Developed a JavaFX iTunes gallery app with search functionality and dynamic content display. " +
        "Implemented API integration for image retrieval and random image replacement playmode. " +
        "Managed UI components", language: "Java, JavaFX", wordToHighlight: ['JavaFX','search','API', 'random','UI', 'play', 'retrieval'], image: itunesGallery, projLink: 'https://github.com/netraA18/ItunesGallery'},
-    {title: "Flappy Bird", description: "Experimented in Processing software, implementing mechanics like collision detection and player controls through object oriented programming", language:"JavaScript", wordToHighlight: ['Processing', 'software,', 'player' , 'controls'], image: flappy, projLink: 'https://github.com/netraA18/FlappyBird'},
-    {title: "ToDoList", description:"Developed a Java-based command line ToDo List program that enables users to enter tasks, view all tasks, and update items flexibly. ", language: "Java", wordToHighlight:['command', 'line', 'update', 'view', 'enter'], image: todoSS, projLink: 'https://github.com/netraA18/ToDoList'},
-    {title: "Brick Breaker", description: "Developed an engaging Brick Breaker game using Processing dynamic ball physics and colorful graphics. Implemented user controls for an intuitive gaming experience, enhancing programming skills and knowledge of game design principles.", language: "JavaScript", wordToHighlight:['dynamic', 'graphics.', 'game', 'design'], image: brickBreaker, projLink: 'https://github.com/netraA18/BrickBreaker'}
+
+   // {title: "Flappy Bird", description: "Experimented in Processing software, implementing mechanics like collision detection and player controls through object oriented programming", language:"JavaScript", wordToHighlight: ['Processing', 'software,', 'player' , 'controls'], image: flappy, projLink: 'https://github.com/netraA18/FlappyBird'},
+   // {title: "ToDoList", description:"Developed a Java-based command line ToDo List program that enables users to enter tasks, view all tasks, and update items flexibly. ", language: "Java", wordToHighlight:['command', 'line', 'update', 'view', 'enter'], image: todoSS, projLink: 'https://github.com/netraA18/ToDoList'},
+   // {title: "Brick Breaker", description: "Developed an engaging Brick Breaker game using Processing dynamic ball physics and colorful graphics. Implemented user controls for an intuitive gaming experience, enhancing programming skills and knowledge of game design principles.", language: "JavaScript", wordToHighlight:['dynamic', 'graphics.', 'game', 'design'], image: brickBreaker, projLink: 'https://github.com/netraA18/BrickBreaker'}
   ];
 
   return (
